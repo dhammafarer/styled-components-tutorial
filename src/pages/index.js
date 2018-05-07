1819758fe569f505131418e4ac23c86223420417
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-05-07 16:57:46 +0800

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,73 @@
+import React from "react"
+import styled, { ThemeProvider } from 'styled-components';
+
+const theme = {
+  main: 'salmon'
+};
+
+const Container = styled.div`
+  margin: 3rem auto;
+  max-width: 600px;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`
+
+const UserWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  margin: 0 auto 12px auto;
+  &:last-child {
+    margin-bottom: 0;
+  }
+  border: 1px solid grey;
+`
+
+const Avatar = styled.img`
+  flex: 0 0 96px;
+  width: 96px;
+  height: 96px;
+  margin: 0;
+`
+const Description = styled.div`
+  flex: 1;
+  margin-left: 18px;
+  padding: 12px;
+`
+const Username = styled.h2`
+  margin: 0 0 12px 0;
+  padding: 0;
+`
+const Excerpt = styled.p`
+  margin: 0;
+  color: ${props => props.theme.main}
+`
+
+const User = props => (
+  <UserWrapper>
+    <Avatar src={props.avatar} alt="" />
+    <Description>
+      <Username>{props.username}</Username>
+      <Excerpt>{props.excerpt}</Excerpt>
+    </Description>
+  </UserWrapper>
+);
+
+export default () => (
+  <ThemeProvider theme={theme}>
+    <Container>
+      <p>Contents</p>
+      <User
+        username="Jane Doe"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/adellecharles/128.jpg"
+        excerpt="I'm Jane Doe. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+      <User
+        username="Bob Smith"
+        avatar="https://s3.amazonaws.com/uifaces/faces/twitter/vladarbatov/128.jpg"
+        excerpt="I'm Bob smith, a vertically aligned type of guy. Lorem ipsum dolor sit amet, consectetur adipisicing elit."
+      />
+    </Container>
+  </ThemeProvider>
+);
