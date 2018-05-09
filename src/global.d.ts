5eea5623917776cfda41a9fc8f44f5a304660a8e
Pawel Lisewski <pawel.lisewski@gmail.com>
2018-05-09 17:01:15 +0800

    write typings for typographyjs

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,1 +1,7 @@
 declare const graphql: (query: TemplateStringsArray) => void
+
+declare module 'typography' {
+  export default class Typography {
+    constructor(theme: {});
+  }
+}
---
+++ src/utils/typography.tsx
@@ -0,0 +1,3 @@
+import Typography from 'typography';
+
+const typography = new Typography({ baseFont: '20px' });
